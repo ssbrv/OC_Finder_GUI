5f6eddea2cad4f74e4b423b87f1b3b639f283fa3
Daniyar Sabirov <daniyar.sabirov@nic.cz>
2025-02-04 01:59:09 +0100

    Fix script

--- scripts/run_oc_finder.bat
+++ scripts/run_oc_finder.bat
@@ -2,12 +2,12 @@
 setlocal
 
 :: Activate Conda Environment
-call "%5" OC_Finder || exit /b 1
+call "%~5" OC_Finder || exit /b 1
 
 :: Change directory to script location
-cd /d "%4" || exit /b 1
+cd /d "%~4" || exit /b 1
 
 :: Run Python script
-python main.py --mode=0 -F="%3" -M=best_model/ema_best.pth.tar --choose=-1 --type=0 --class=2 --height=%1 --width=%2
+python main.py --mode=0 -F="%~3" -M="best_model/ema_best.pth.tar" --choose=-1 --type=0 --class=2 --height=%~1 --width=%~2
 
 endlocal
